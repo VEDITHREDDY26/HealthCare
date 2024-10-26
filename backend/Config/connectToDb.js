8655a5eae6b9532ceed30fed2c16170d532c543c
srilatha-jujjavarapu <srilathajujjavarapu@gmail.com>
2024-10-26 21:34:03 +0530

    Db commit

--- backend/Config/connectToDb.js
+++ backend/Config/connectToDb.js
@@ -5,7 +5,7 @@ const connectToDb = async () => {
     try {
         // await mongoose.connect(process.env.MONGO_URI);
         
-        await mongoose.connect("mongodb://localhost:27017/");
+        await mongoose.connect(`${process.env.URI}`);
 
 
         console.log("MongoDB Connected");
