844b65931933ae67bffb23e44b640be4b3c93824
VEDITHREDDY26 <vedithreddykommula@gmail.com>
2024-10-27 14:19:03 +0530

    navbar upated

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,9 +1,9 @@
-import React, { useContext } from 'react';
-import { Cross } from 'lucide-react';
+import React, { useContext } from "react";
+import { Cross } from "lucide-react";
 import { toast, ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-import { Link, useNavigate } from 'react-router-dom';
-import { Auth } from '../Contexts/AuthContext';
+import "react-toastify/dist/ReactToastify.css";
+import { Link, useNavigate } from "react-router-dom";
+import { Auth } from "../Contexts/AuthContext";
 
 const Navbar = () => {
   const { user } = useContext(Auth);
@@ -21,7 +21,7 @@ const Navbar = () => {
   const handleFindDoctor = () => {
     if (user) {
       if (role === "Patient") {
-        nav('/dashboard/patient/bookAppointment');
+        nav("/dashboard/patient/bookAppointment");
       } else {
         toast.error("Only Patients have access");
       }
@@ -43,24 +43,61 @@ const Navbar = () => {
   };
 
   return (
-    <div className='sticky top-0 z-50 drop-shadow-lg flex items-center w-full h-20 bg-gradient-to-r from-pink-200 via-purple-300 to-indigo-200'>
+    <div className="sticky top-0 z-50 drop-shadow-lg flex items-center w-full h-20 bg-gradient-to-r from-pink-200 via-purple-300 to-indigo-200">
       <ToastContainer />
-      <div className='flex items-center pl-10'>
-        <Cross className='w-16 h-16 cursor-pointer' />
-        <Link to='/' className='font-bold text-xl cursor-pointer ml-2'>Celestial Care</Link> {/* Increased font size */}
+      <div className="flex items-center pl-10">
+        <Cross className="w-16 h-16 cursor-pointer" />
+        <Link to="/" className="font-bold text-xl cursor-pointer ml-2">
+          Celestial Care
+        </Link>{" "}
+        {/* Increased font size */}
       </div>
-      <ul className='flex justify-center items-center gap-10 flex-grow'>
-        <Link to='/' className='hover:border-b-2 border-slate-600 cursor-pointer text-md'>Home</Link>
-        <li onClick={handleAbout} className='hover:border-b-2 border-slate-600 cursor-pointer'>About us</li>
-        <li onClick={handleContact} className='hover:border-b-2 border-slate-600 cursor-pointer'>Contact us</li>
-        <li onClick={handleFindDoctor} className='hover:border-b-2 border-slate-600 cursor-pointer'>Find a Doctor</li>
-        {user !== null ? <Link to='/dashboard' className='hover:border-b-2 border-slate-600 cursor-pointer'>Dashboard</Link> : null}
+      <ul className="flex justify-center items-center gap-10 flex-grow">
+        <Link
+          to="/"
+          className="hover:border-b-2 border-slate-600 cursor-pointer text-md"
+        >
+          Home
+        </Link>
+        <li
+          onClick={handleAbout}
+          className="hover:border-b-2 border-slate-600 cursor-pointer"
+        >
+          About us
+        </li>
+        <li
+          onClick={handleContact}
+          className="hover:border-b-2 border-slate-600 cursor-pointer"
+        >
+          Contact us
+        </li>
+        <li
+          onClick={handleFindDoctor}
+          className="hover:border-b-2 border-slate-600 cursor-pointer"
+        >
+          Find a Doctor
+        </li>
+        {user !== null ? (
+          <Link
+            to="/dashboard"
+            className="hover:border-b-2 border-slate-600 cursor-pointer"
+          >
+            Dashboard
+          </Link>
+        ) : null}
       </ul>
-      <button onClick={() => user !== null ? handleLogout() : handleLogin()} className='shadow-lg hover:bg-blue-800 w-20 h-9 bg-blue-500 rounded-full text-white'>
+      {/* <button onClick={() => user !== null ? handleLogout() : handleLogin()} className='shadow-lg hover:bg-blue-800 w-20 h-9 bg-blue-500 rounded-full text-white'>
+        {user !== null ? "Log Out" : "Log In"}
+      </button> */}
+
+      <button
+        onClick={() => (user !== null ? handleLogout() : handleLogin())}
+        className="shadow-lg hover:bg-blue-800 w-20 h-9 bg-blue-500 rounded-full text-white mr-[55px]"
+      >
         {user !== null ? "Log Out" : "Log In"}
       </button>
     </div>
   );
-}
+};
 
 export default Navbar;
