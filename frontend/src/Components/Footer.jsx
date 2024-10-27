232e661787e119e1ba4d51460102445bfd1446a1
VEDITHREDDY26 <vedithreddykommula@gmail.com>
2024-10-27 14:19:39 +0530

    updated footer

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -13,16 +13,27 @@
 
 // export default Footer
 
+// import React from 'react';
 
+// const Footer = () => {
+//   return (
+//     <footer className="border-t-2 border-slate-400 shadow-lg flex justify-center text-slate-600 items-center text-sm h-20 bg-gradient-to-r from-blue-200 via-purple-200 to-pink-200 ">
+//           {/* <footer className="border-t-2 border-slate-400 shadow-lg flex justify-center text-slate-600 items-center text-sm h-20 bg-gradient-to-r from-indigo-100 from-10% via-sky-200 via-30% to-emerald-100"> */}
 
-import React from 'react';
+//       Copyright<span className="font-bold text-2xl mx-1">©</span> 2024 CelestialCare Inc.
+//     </footer>
+//   );
+// };
+
+// export default Footer;
+
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="border-t-2 border-slate-400 shadow-lg flex justify-center text-slate-600 items-center text-sm h-20 bg-gradient-to-r from-blue-200 via-purple-200 to-pink-200 ">
-          {/* <footer className="border-t-2 border-slate-400 shadow-lg flex justify-center text-slate-600 items-center text-sm h-20 bg-gradient-to-r from-indigo-100 from-10% via-sky-200 via-30% to-emerald-100"> */}
-
-      Copyright<span className="font-bold text-2xl mx-1">©</span> 2024 CelestialCare Inc.
+    <footer className="border-t-2 border-slate-400 shadow-lg flex justify-center text-slate-600 items-center text-sm h-20 bg-gradient-to-r from-blue-200 via-purple-200 to-pink-200 mt-auto">
+      Copyright <span className="font-bold text-2xl mx-1">©</span> 2024
+      CelestialCare Inc.
     </footer>
   );
 };
