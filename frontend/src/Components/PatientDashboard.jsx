85433505d8f908feada1922d8abf84b5b2b2ccf0
VEDITHREDDY26 <vedithreddykommula@gmail.com>
2024-11-06 17:42:31 +0530

    updated name

--- frontend/src/Components/PatientDashboard.jsx
+++ frontend/src/Components/PatientDashboard.jsx
@@ -21,7 +21,7 @@ const PatientDashboard = () => {
   return (
     <>
       <div className="flex flex-col justify-center items-center min-h-screen bg-gradient-to-r from-blue-200 via-purple-200 to-pink-200 p-6">
-        <h1 className="text-3xl font-bold text-center text-gray-800 mb-4">Welcome, Patient!</h1>
+        <h1 className="text-3xl font-bold text-center text-gray-800 mb-4">Welcome To Celestial Care!</h1>
         <p className="text-lg text-center text-gray-700 mb-8">We’re here to support your health and well-being.</p>
         <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full text-center">
           <h2 className="text-xl font-semibold text-gray-800 mb-4">Quote of the Day</h2>
